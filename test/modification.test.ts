645b023b1f4c4879fbf47a7fea5f282350a7a3cd
Dave Townsend <dtownsend@oxymoronical.com>
2020-07-19 06:42:16 -0700

    Test tag modification

--- test/modification.test.ts
+++ test/modification.test.ts
@@ -84,3 +84,76 @@ test("Update attributes", async (): Promise<void> => {
     project: "Foo",
   });
 });
+
+test("Tags", async (): Promise<void> => {
+  await buildTaskDb([
+    [
+      "add",
+      "project:Foo",
+      "tag:MyTag,OtherTag",
+      "the task",
+    ],
+  ]);
+
+  let warrior = await tw();
+
+  let tasks = await warrior.list();
+  expect(tasks).toHaveLength(1);
+
+  let task = tasks[0];
+  expect(task.isModified).toBeFalsy();
+  let uuid = task.uuid;
+
+  expect(toJSON(task)).toEqual({
+    uuid: expect.stringMatching(UUID_REGEX),
+    status: "pending",
+    entry: expect.toBeCloseToDate(),
+    description: "the task",
+    tags: ["MyTag", "OtherTag"],
+    project: "Foo",
+  });
+
+  task.tags.add("foo");
+  expect(task.isModified).toBeTruthy();
+  task.tags.delete("foo");
+  expect(task.isModified).toBeFalsy();
+  task.tags.delete("MyTag");
+  expect(task.isModified).toBeTruthy();
+  task.tags.add("foo");
+
+  expect(toJSON(task)).toEqual({
+    uuid,
+    status: "pending",
+    entry: expect.toBeCloseToDate(),
+    description: "the task",
+    tags: ["OtherTag", "foo"],
+    project: "Foo",
+  });
+
+  await task.save();
+  expect(task.isModified).toBeFalsy();
+
+  expect(toJSON(task)).toEqual({
+    uuid,
+    status: "pending",
+    entry: expect.toBeCloseToDate(),
+    description: "the task",
+    tags: ["OtherTag", "foo"],
+    project: "Foo",
+  });
+
+  tasks = await warrior.list();
+  expect(tasks).toHaveLength(1);
+
+  task = tasks[0];
+  expect(task.isModified).toBeFalsy();
+
+  expect(toJSON(task)).toEqual({
+    uuid,
+    status: "pending",
+    entry: expect.toBeCloseToDate(),
+    description: "the task",
+    tags: ["OtherTag", "foo"],
+    project: "Foo",
+  });
+});
